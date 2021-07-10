9cf58fa11982ea3f9565ec7df6883026e9baeb1c
TCarmine <kappa85t@hotmail.it>
2021-07-10 12:29:05 +0200

    achieved title props overlay over image as background plus another background

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,14 +12,26 @@ const Projects = () => {
         {items.map((item) => {
           const { id, title, subtitle, description, imgSrc } = item
           return (
-            <div key={id}>
-              <p className="block text-center text-lg">{title}</p>
-              <img src={`${imgSrc}`} alt={subtitle} className="p-5 bg-green-500 rounded opacity-70"/>
-              <small className="block text-center text-white
-              font-semibold text-lg">
-                {description}
-              </small>
-            </div>
+            <div key={id} class="relative ">
+              <div className="absolute inset-0 z-10 text-center flex flex-col items-center justify-center ">
+                <p className="block text-center text-lg">{title}</p> 
+                <small className="block text-center text-white font-semibold text-lg opacity-0 hover:opacity-100">
+                  {description}
+                </small>
+              </div>  
+              <div >
+              {/*<a className="absolute inset-0 z-10 bg-white text-center flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-opacity-90 duration-300">
+                <h1 className="tracking-wider">{title}</h1>
+                <p  className="block text-center text-lg">{description}</p>
+              </a>
+              <a href="#" className="relative">
+                <div className="h-48 flex flex-wrap content-center">
+                  <img src={`${imgSrc}`} className="p-5 bg-green-500 rounded opacity-70" alt={subtitle}/>
+                </div>
+              </a> */}
+                <img src={`${imgSrc}`} alt={subtitle} className="p-5 bg-green-500 rounded opacity-50"/>
+              </div>
+            </div>  
           )
         })}
       </section>
