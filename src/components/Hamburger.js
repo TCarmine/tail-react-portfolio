0c4342c41c06b935005c623e293ac81abf3a3818
TCarmine <kappa85t@hotmail.it>
2021-08-01 18:48:14 +0200

    refactored hamburger style with object variable

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,24 +1,26 @@
 import React from 'react'
 
+
+
 const Hamburger = ({isOpen}) => {
+  const burger1 = {
+    transform: `${isOpen ? 'rotate(45deg)':'rotate(0)'}` 
+  }
+  
+  const burger2 = {
+    opacity: `${isOpen? 0 : 1}`
+  }
+  const burger3= {
+    transform: `${isOpen ? 'rotate(-45deg)':'rotate(0)'}`
+  }
+
   return (
     <>
       <div className="hamburger">
-        <div className="burger burger1" />
-        <div className="burger burger2" />
-        <div className="burger burger3" />
+        <div className="burger burger1" style={burger1}/>
+        <div className="burger burger2" style={burger2}/>
+        <div className="burger burger3" style={burger3}/>
       </div>
-      <style jsx>{`
-          .burger1{
-            transform: ${isOpen ? 'rotate(45deg)':'rotate(0)'}
-          }
-          .burger2{
-            opacity: ${isOpen? 0 : 1}
-          }
-          .burger3{
-            transform: ${isOpen ? 'rotate(-45deg)':'rotate(0)'}
-          }
-        `}</style>
     </>  
   )
 }
