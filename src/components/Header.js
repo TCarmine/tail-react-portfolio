9605328df3ab8b4a29319e74e2fc69ce299296d3
TCarmine <kappa85t@hotmail.it>
2021-08-07 11:21:09 +0200

    improved navbar buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
         <header className="flex flex-wrap justify-between md:space-x-4 p-10 w-full text-white relative">
           <div className="logo"> 
             <h2>
-              <Link to="/" className="text-black text-3xl lg:text-7xl">Carmine Tambascia</Link>
+              <Link to="/" className="text-black text-3xl lg:text-5xl">Carmine Tambascia</Link>
             </h2>
            {/* <img class="h-8" src="" alt="Themes.dev Logo" title=""></img> for image logo*/}   
           </div>
@@ -23,7 +23,7 @@ const Header = () => {
           <nav className={`nav-links ${hamburgerOpen ? 'open' : ''}`}>
             <ul className="flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg p-6 md:p-0">
             <li className="bg-blue-400 rounded-full 
-               hover:bg-blue-500 mr-5 p-1 px-4">
+               hover:bg-blue-500 mr-0 p-1 px-4">
                 <Link to="/VisualCV">Timeline</Link>
               </li>
               <li className="bg-blue-400 rounded-full 
