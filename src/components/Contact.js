207565bc002f35ece0de54ae5cc86fb45701f34c
TCarmine <kappa85t@hotmail.it>
2021-08-12 06:44:16 +0200

    thanks html page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,7 +38,7 @@ const Contact = () => {
                 className="flex flex-col
                 bg-blue-200 px-10 py-5 rounded"
                 method="POST"
-                action="/"
+                action="/contact_thanks"
                 data-netlify="true" 
               >
               <input 
