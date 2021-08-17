974ce966c989ae964b5ace4adf25606397aa4aa1
TCarmine <kappa85t@hotmail.it>
2021-08-17 22:29:14 +0200

    missing hidden input in JSX

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,7 +58,8 @@ const Contact = () => {
               bg-blue-200 px-10 py-5 rounded"
               method="POST"
               action="/thank-you.html"
-              onSubmit={handleSubmit, {setSubmitted:true}}>                 
+              onSubmit={handleSubmit, {setSubmitted:true}}>   
+              <input type="hidden" name="form-name" value="contact" />              
             <input 
               id="first-name"
               type="text" 
@@ -73,46 +74,12 @@ const Contact = () => {
               placeholder-blue-400 font-semibold" >
             </input>
             {submitted && !inputs.firstName && <span id='first-name-error'>Please enter a first name</span>}
-            {/* <input 
-              type="email" 
-              name="email" 
-              id="email_id" 
-              placeholder="Enter your email address" 
-              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-              className="py-2 px-4 mb-5 rounded border border-solid border-blue-500 
-              placeholder-blue-400 font-semibold">
-            </input>
-            <input 
-              type="tel"
-              name="mobile_number"
-              placeholder="Mobile number" 
-              className="py-2 px-4 mb-5 rounded border border-solid border-blue-500 
-              placeholder-blue-400 font-semibold" >
-              </input>    
-            <select name="selection"
-              className="py-2 px-4 mb-5 rounded border border-solid border-blue-500 
-                placeholder-blue-400 font-semibold">
-                <option value="Mr">Mr</option>
-                <option value="Mrs">Mrs</option>
-                <option value="Miss">Miss</option>
-                <option value="Dr">Dr</option>
-            </select> 
-            <textarea 
-              name="textarea" 
-              id="textarea_id" 
-              cols="30"
-              rows="10"
-              placeholder="Leave me a message with a short introduction" 
-              className="py-2 px-4 mb-5 rounded border border-solid border-blue-500 
-              placeholder-blue-400 font-semibold" >
-            </textarea>*/}
             <input 
               type="submit" 
               value="Submit message" 
               className="bg-blue-400 text-white font-bold
                 tracking-wider py-2 rounded cursor-pointer 
                 transition-all hover:bg-blue-800">
-
             </input> 
             </form>
             {showSuccess &&<div class='success-message'>Success! Thank you for registering</div>}
